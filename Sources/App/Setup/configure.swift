436fdd26cbca1ac0cbf56508805015ac60b6805b
brien84 <houseinblues@gmail.com>
2019-10-28 20:09:53 +0200

    Remove boilerplate files

--- Sources/App/Setup/configure.swift
+++ Sources/App/Setup/configure.swift
@@ -3,24 +3,24 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
+    /// Register providers first
     try services.register(FluentSQLiteProvider())
 
-    // Register routes to the router
+    /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
-    // Configure a SQLite database
+    /// Configure a SQLite database
     let directoryConfig = DirectoryConfig.detect()
     let database = try SQLiteDatabase(storage: .file(path: "\(directoryConfig.workDir)articles.db"))
 
-    // Register the configured SQLite database to the database config.
+    /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: database, as: .sqlite)
     services.register(databases)
 
-    // Configure migrations
+    /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Article.self, database: .sqlite)
     services.register(migrations)
